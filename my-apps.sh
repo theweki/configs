6e12cfbad53f12704f139ffb49aed043e53ed4ee
Rituraj Pandey <rp.weki.24@gmail.com>
2024-06-30 11:44:17 +0530

    $

---
+++ my-apps.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo pacman --noconfirm -S nodejs npm jdk-openjdk docker
--- utilities.sh
+++ utilities.sh
@@ -8,6 +8,4 @@ sudo systemctl enable auto-cpufreq.service
 sleep 0.5
 
 # Graphic Drivers
-# pacman --noconfirm -S mesa vulkan-radeon vulkan-intel vulkan-nouveau xf86-video-amdgpu xf86-video-ati xf86-video-intel xf86-video-nouveau xf86-video-vmware intel-media-driver libva-intel-driver libva-mesa-driver
-
-# My Apps
+sudo pacman --noconfirm -S mesa vulkan-radeon vulkan-intel vulkan-nouveau xf86-video-amdgpu xf86-video-ati xf86-video-intel xf86-video-nouveau xf86-video-vmware intel-media-driver libva-intel-driver libva-mesa-driver
